8e4e83a2b6781ed0cfba6791139d40e79f39fbd3
pii5656 <microapple56@gmail.com>
2015-05-25 23:57:07 +0900

    fix-end-of-audio-bug

--- app/src/main/java/com/example/pii5656/shadowingmanager/MainActivity.java
+++ app/src/main/java/com/example/pii5656/shadowingmanager/MainActivity.java
@@ -1,53 +1,36 @@
 package com.example.pii5656.shadowingmanager;
 
-import com.example.pii5656.shadowingmanager.MediaPlayerSample;
-import com.example.pii5656.shadowingmanager.Record;
 import android.app.Activity;
-import com.example.pii5656.shadowingmanager.FileSelect.OnFileSelectDialogListener;
 import android.content.Intent;
 import android.media.MediaPlayer;
-import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Message;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.util.Log;
 import android.widget.ImageButton;
-import android.widget.Toast;
 import java.io.File;
-import android.app.Activity;
-import android.os.Bundle;
 import android.os.Environment;
 import android.widget.TextView;
-
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-//import java.util.logging.Handler;
 import android.os.Handler;
-
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class MainActivity extends Activity implements View.OnClickListener, OnFileSelectDialogListener,OnSeekBarChangeListener, Runnable{
+public class MainActivity extends Activity implements View.OnClickListener,OnSeekBarChangeListener, Runnable{
 
-    ImageButton play_button = null, stop_button = null, rec_start_button = null, rec_stop_button = null, rec_play_start_button = null, rec_play_stop_button = null,
+    ImageButton play_button = null, rec_start_button = null, rec_play_start_button = null,
                 fast_button = null, back_button = null;
     TextView textview = null;
     MediaPlayer mp = new MediaPlayer();
     MediaPlayer rec_mp = new MediaPlayer();
     Record rec = new Record();//Instantiate RecordClass
-    MediaPlayerSample play = new MediaPlayerSample();//Instantiate MediaPlayerSampleClass
-    private Menu mainMenu;
-    private static final int MENU_ID_MENU1 = (Menu.FIRST + 1);
-    private static final int MENU_ID_MENU2 = (Menu.FIRST + 2);
-    FileSelect fileselect = new FileSelect();
     private final static int CHOSE_FILE_CODE = 12345;
 
     private SeekBar seekBar;
@@ -64,29 +47,23 @@ public class MainActivity extends Activity implements View.OnClickListener, OnFi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //mp = MediaPlayer.create(this, R.raw.jobs4);
 
         play_button = (ImageButton) findViewById(R.id.PlayButton);
         play_button.setOnClickListener(this);
-        //stop_button = (ImageButton) findViewById(R.id.StopButton);
-        //stop_button.setOnClickListener(this);
         rec_start_button = (ImageButton) findViewById(R.id.startRecordButton);
         rec_start_button.setOnClickListener(this);
-        //rec_stop_button = (ImageButton) findViewById(R.id.stopRecordButton);
-        //rec_stop_button.setOnClickListener(this);
         rec_play_start_button = (ImageButton) findViewById(R.id.startPlayButton);
         rec_play_start_button.setOnClickListener(this);
-        //rec_play_stop_button = (ImageButton) findViewById(R.id.stoPlayButton);
-        //rec_play_stop_button.setOnClickListener(this);
         back_button = (ImageButton) findViewById(R.id.BackButton);
         back_button.setOnClickListener(this);
         fast_button = (ImageButton) findViewById(R.id.FastButton);
         fast_button.setOnClickListener(this);
         textview = (TextView) findViewById(R.id.textView);
         textview.setText("Open your audio file.");
-        //録音の保存先のディレクトリの作成
-        //String saveDir = Environment.getExternalStorageDirectory().getPath() + "/test";
+        seekBar = (SeekBar)findViewById(R.id.seekBar1);
+        seekBar.setProgress(0);
 
+        //録音の保存先のディレクトリの作成
         String saveDir = Environment.getExternalStorageDirectory().getPath() + "/Shadowing/";
         File folder = new File(saveDir);
         if (!folder.exists()) {
@@ -105,46 +82,26 @@ public class MainActivity extends Activity implements View.OnClickListener, OnFi
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.PlayButton:
-                if (!mp.isPlaying()) {
-                    Log.v("AudioRecord", "saisei");
-                    // MediaPlayerの再生
-                    //play_button.setText("Pause");
-                    //mp.start();
+                if(mp == null){
+                    break;
+                }else if (!mp.isPlaying()) {
                     this.findViewById(R.id.PlayButton).setActivated(true);
-                    start();/*
+                    start();
                     mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                         //音源終了後のアクション
                         @Override
                         public void onCompletion(MediaPlayer mp) {
                             findViewById(R.id.PlayButton).setActivated(false);
-                            mp.pause();
+
                             mp.seekTo(0);
                         }
-                    });*/
+                    });
                 } else {
-                    Log.v("AudioRecord", "ichijiteisi");
-                    // MediaPlayerの一時停止
-                    //play_button.setText("Start");
-                    //mp.pause();
                     this.findViewById(R.id.PlayButton).setActivated(false);
                     pause();
                 }
                 break;
-            /*
-            case R.id.StopButton:
-                if (mp.isPlaying()) {
-                    // MediaPlayerの停止
-                    //play_button.setText("Start");
-                    //mp.stop();
-                    stop();
-                    try {
-                        // MediaPlayerの準備
-                        mp.prepare();
-                    } catch (Exception e) {
-                    }
-                }
-                break;
-             */
+
             case R.id.startRecordButton:
                 if (flag_record == false) {
                     if (mp != null) {
@@ -178,22 +135,6 @@ public class MainActivity extends Activity implements View.OnClickListener, OnFi
                 }
                 Log.v("test", "stop recording!");
                 break;
-            /*case R.id.stopRecordButton:
-                if(mp.isPlaying()){
-                    mp.stop();
-                    try{
-                        mp.prepare();
-                    } catch (IOException e){
-                        //TODO 例外処理
-                    }
-                }
-                rec.stopRecord();
-                try{
-                    rec_mp.setDataSource(savePath);
-                    rec_mp.prepare();
-                } catch(IOException e){
-                }
-                break;*/
             case R.id.startPlayButton:
                 if(isrecorded == false) {
                     break;
@@ -266,26 +207,17 @@ public class MainActivity extends Activity implements View.OnClickListener, OnFi
         }
     }
 
-
     //メニューボタンが押された際に表示されるアイテムを追加する
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         final MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
-        mainMenu = menu;
         return super.onCreateOptionsMenu(menu);
     }
 
     //メニューで要素が選択された時の動作
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        //Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
-        //Log.v("a","menu selected");
-                //Intent i = new Intent(getApplicationContext(), FileSelect.class);
-                //startActivity(i);
-                Log.v("a","selected");
-                //onClickFileSelect(this);
-
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
         startActivityForResult(intent, CHOSE_FILE_CODE);
@@ -306,7 +238,6 @@ public class MainActivity extends Activity implements View.OnClickListener, OnFi
             } if(flag_record == true){
                 rec.stop();
             }
-
             //選択した音源ファイルのパス取得
             String filePath = data.getDataString();
             //選択したのファイル名を表示
@@ -325,8 +256,7 @@ public class MainActivity extends Activity implements View.OnClickListener, OnFi
                 mp.setDataSource(this, Uri.parse(filePath));
                 mp.prepare();
                 //mp.start();
-                seekBar = (SeekBar)findViewById(R.id.seekBar1);
-                seekBar.setProgress(0);
+
                 seekBar.setMax(mp.getDuration());
                 seekBar.setOnSeekBarChangeListener(this);
             } catch (IOException e) {
@@ -342,9 +272,6 @@ public class MainActivity extends Activity implements View.OnClickListener, OnFi
         if (action == KeyEvent.ACTION_UP) {
             // メニュー表示
             if (keyCode == KeyEvent.KEYCODE_MENU) {
-                if (mainMenu != null) {
-                    mainMenu.performIdentifierAction(R.id.overflow_options, 0);
-                }
                 return true;
             }
         } else {
@@ -352,7 +279,6 @@ public class MainActivity extends Activity implements View.OnClickListener, OnFi
         }
         return true;
     }
-
 
     // Run the thread using for seekBar
     public void run(){
@@ -372,62 +298,15 @@ public class MainActivity extends Activity implements View.OnClickListener, OnFi
         }
     };
 
-    public void stopRunning(){
-        running = false;
-    }
-
     // SeekBar-control
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
     }
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
-        //mp.seekTo(seekBar.getProgress());
     }
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
-        //int msec = seekBar.getProgress() * mp.getDuration() /  100;
         mp.seekTo(seekBar.getProgress());
-        //mp.seekTo(11100);
-        Log.e("test",String.valueOf(mp.getDuration()));
     }
-
-    /**
-     * ファイル選択イベント
-     */
-    public void onClickFileSelect(View view) {
-
-        // ファイル選択ダイアログを表示
-        FileSelect dialog = new FileSelect();
-        dialog.setOnFileSelectDialogListener(this);
-
-        // 表示
-        dialog.show(Environment.getExternalStorageDirectory().getPath());
-    }
-
-    /**
-     * ファイル選択完了イベント
-     */
-    @Override
-    public void onClickFileSelect(File file) {
-
-        if (file != null) {
-            // 選択ファイルを設定
-            //((TextView)findViewById(R.id.text)).setText(file.getPath() + file.getName());
-        }
-    }
-
-/*
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        try {
-            if (requestCode == CHOSE_FILE_CODE && resultCode == RESULT_OK) {
-                String filePath = data.getDataString().replace("file://", "");
-                String decodedfilePath = URLDecoder.decode(filePath, "utf-8");
-                imageText.setText(decodedfilePath);
-            } catch (UnsupportedEncodingException e) {
-                // いい感じに例外処理
-            }
-        }
-    }*/
 }
